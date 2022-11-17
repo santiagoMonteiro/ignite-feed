d3e7dd4eb563a7766fb4c0050953685ad7eac67c
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2022-11-17 12:04:05 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Post } from "./components/Post";
+
+export function App() {
+  return (
+    <>
+      <div>Hello World</div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+}
