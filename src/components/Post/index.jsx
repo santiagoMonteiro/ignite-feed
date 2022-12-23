3ba34a43ed83a6c0ef6195ceaaf1ddbdc6ed605d
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2022-12-22 22:02:43 -0400

    adding formatted dates

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,37 +1,54 @@
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
 import { Avatar } from "../Avatar";
 import { Comment } from "../Comment";
 import styles from "./styles.module.scss";
 
-export function Post({ author, content }) {
+export function Post({ author, content, publishedAt }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
+
+  const publishedDateRelativeFromNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/santiagoMonteiro.png" />
+          <Avatar src={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
-            <strong>Santiago Monteiro</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-12-14 14:57:00">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeFromNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          👉 <a>jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a>#novoprojeto</a> <a>#nlw</a> <a>#rocketseat</a>
-        </p>
+        {content.map((line) =>
+          line.type === "paragraph" ? (
+            <p>{line.content}</p>
+          ) : (
+            <p>
+              <a href="#">{line.content}</a>
+            </p>
+          )
+        )}
       </div>
 
       <form className={styles.commentForm}>
